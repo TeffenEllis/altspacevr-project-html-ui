143d80cc4b5b4196d417ac6bff7c0031a4559b36
Teffen Ellis (Eric) <teffen@nirri.us>
2015-12-13 20:35:59 -0800

    Flesh out project boilerplate.
    - Webpack
    - React
    - Redux
    - Babel
    - Vendor data adapter

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import "babel-polyfill"
+import "./index.styl"
+
+import React from "react"
+import {render} from "react-dom"
+import Application from "./containers/application"
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(<Application />, document.querySelector("main"))
+})
