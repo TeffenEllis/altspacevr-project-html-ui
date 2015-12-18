592fa4b19272d21a220a44890f01f6049b73b932
Teffen Ellis (Eric) <teffen@nirri.us>
2015-12-18 01:26:37 -0800

    Fix issue where concat is used over replace.

--- app/resources/reducers/spaces.js
+++ app/resources/reducers/spaces.js
@@ -2,7 +2,7 @@ import $ from "resources/constants"
 
 export default function spaces(state = [], action) {
   if (action.type === $.ADD_SPACES) {
-    return state.concat(action.spaces)
+    return action.spaces
   }
 
   return state
--- app/resources/reducers/users.js
+++ app/resources/reducers/users.js
@@ -2,7 +2,7 @@ import $ from "resources/constants"
 
 export default function users(state = [], action) {
   if (action.type === $.ADD_USERS) {
-    return state.concat(action.users)
+    return action.users
   }
 
   return state
