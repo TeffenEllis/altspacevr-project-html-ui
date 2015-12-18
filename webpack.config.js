d9dccecb5589bf60d2a7c9c3674cb013e552f867
Teffen Ellis (Eric) <teffen@nirri.us>
2015-12-18 10:43:00 -0800

    Allow production and development builds.

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ exports.module = {
 exports.output = {
   filename: "[name].js",
   path: resolve(__dirname, "dist"),
-  publicPath: "./",
+  publicPath: ENVIRONMENT === "production" ? "./" : "/",
   sourceMapFilename: "[name].map"
 }
 
